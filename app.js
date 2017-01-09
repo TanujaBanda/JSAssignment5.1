fa8e9ec765b0a14ad4953c90cac49e054b99602c
TanujaBanda <tanujakumari24m@gmail.com>
2017-01-09 11:51:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var name="Tanuja";
 var age="21";
-var dateofbirth="24 May,2016";
+var dateofbirth="24 May,1995";
 var placeofbirth="Koppal";
 console.log("Name="+name+"\nAge="+age+"\nDateofbirth="+dateofbirth+"\nPlaceofbirth="+placeofbirth);
