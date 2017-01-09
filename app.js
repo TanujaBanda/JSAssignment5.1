a5fb535f87a338c9997d8179c7b945394a4ac3c1
TanujaBanda <tanujakumari24m@gmail.com>
2017-01-09 11:40:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var name="Tanuja";
+var age="21";
+var dateofbirth="24 May,2016";
+var placeofbirth="Koppal";
+console.log("Name="+name+"\nAge="+age+"\nDateofbirth="+dateofbirth+"\nPlaceofbirth="+placeofbirth);
